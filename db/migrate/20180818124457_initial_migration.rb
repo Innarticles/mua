42471f80db962005bf0ddc1c8037c84f4cebd45f
Innocent Udeogu <innocentudeh@gmail.com>
2018-09-25 17:03:29 +0100

    done

--- db/migrate/20180818124457_initial_migration.rb
+++ db/migrate/20180818124457_initial_migration.rb
@@ -1,4 +1,4 @@
-class InitialMigration < ActiveRecord::Migration[5.1]
+class InitialMigration < ActiveRecord::Migration[5.2]
   def change
   end
 end
--- db/migrate/20180818130750_create_entries.rb
+++ db/migrate/20180818130750_create_entries.rb
@@ -1,11 +1,10 @@
-class CreateEntries < ActiveRecord::Migration[5.1]
+class CreateEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :entries do |t|
       t.string :name
       t.text :content
       t.string :category
       t.string :ig_handle
-
       t.timestamps
     end
   end
